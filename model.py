ac2432f0bfc97d138770504ad2b77b772229b8b7
Kwanda Silekwa <kwanda9700@gmail.com>
2021-06-25 15:16:31 +0200

    update

---
+++ model.py
@@ -0,0 +1,157 @@
+"""
+
+    Helper functions for the pretrained model to be used within our API.
+
+    Author: Explore Data Science Academy.
+
+    Note:
+    ---------------------------------------------------------------------
+    Plase follow the instructions provided within the README.md file
+    located within this directory for guidance on how to use this script
+    correctly.
+
+    Importantly, you will need to modify this file by adding
+    your own data preprocessing steps within the `_preprocess_data()`
+    function.
+    ----------------------------------------------------------------------
+
+    Description: This file contains several functions used to abstract aspects
+    of model interaction within the API. This includes loading a model from
+    file, data preprocessing, and model prediction.  
+
+"""
+
+# Helper Dependencies
+import numpy as np
+import pandas as pd
+import pickle
+import json
+from sklearn.preprocessing import StandardScaler
+
+# Filter for only apples.
+def keep_golden(df):
+    """
+    Filter the dataframe on the Commodities column to only keep
+    the APPLE GOLDEN DELICIOUS category.
+    
+    Parameters
+    ----------
+    df : DataFrame
+        Input DataFrame
+    Returns
+    -------
+    df: DataFrame
+        Altered DataFrame with only APPLE GOLDEN DELICIOUS
+        
+    """
+    df = df.copy()
+    df = df[df["Commodities"] == "APPLE GOLDEN DELICIOUS"]
+    df = df.drop(["Commodities"], axis=1)
+    return df 
+
+# Scaling function.
+def scaling(X):
+    """
+    Scale the values of DataFrame according to the
+    Standardisation process.
+    
+    
+    Parameters
+    ----------
+    X : DataFrame
+        Input DataFrame
+    Returns
+    -------
+    X: DataFrame
+        Scaled DataFrame
+    """
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    X_standardise = pd.DataFrame(X_scaled, columns=X.columns)
+    return X_standardise
+
+
+def _preprocess_data(data):
+    """Private helper function to preprocess data for model prediction.
+
+    NB: If you have utilised feature engineering/selection in order to create
+    your final model you will need to define the code here.
+
+
+    Parameters
+    ----------
+    data : str
+        The data payload received within POST requests sent to our API.
+
+    Returns
+    -------
+    Pandas DataFrame : <class 'pandas.core.frame.DataFrame'>
+        The preprocessed data, ready to be used our model for prediction.
+
+    """
+    # Convert the json string to a python dictionary object
+    feature_vector_dict = json.loads(data)
+    # Load the dictionary as a Pandas DataFrame.
+    feature_vector_df = pd.DataFrame.from_dict([feature_vector_dict])
+
+    # ---------------------------------------------------------------
+    # NOTE: You will need to swap the lines below for your own data
+    # preprocessing methods.
+    #
+    # The code below is for demonstration purposes only. You will not
+    # receive marks for submitting this code in an unchanged state.
+    # ---------------------------------------------------------------
+
+    # ----------- Replace this code with your own preprocessing steps --------
+
+    keep = ["Weight_Kg", "Low_Price", "High_Price", "Sales_Total", "Total_Qty_Sold", "Total_Kg_Sold", "Stock_On_Hand"]
+
+    feature_vector_df = keep_golden(feature_vector_df)
+    feature_vector_df = feature_vector_df[keep]
+    predict_vector = scaling(feature_vector_df)
+    print(predict_vector)
+
+    # ------------------------------------------------------------------------
+
+    return predict_vector
+
+def load_model(path_to_model:str):
+    """Adapter function to load our pretrained model into memory.
+
+    Parameters
+    ----------
+    path_to_model : str
+        The relative path to the model weights/schema to load.
+        Note that unless another file format is used, this needs to be a
+        .pkl file.
+
+    Returns
+    -------
+    <class: sklearn.estimator>
+        The pretrained model loaded into memory.
+
+    """
+    return pickle.load(open(path_to_model, 'rb'))
+
+def make_prediction(data, model):
+    """Prepare request data for model prediciton.
+
+    Parameters
+    ----------
+    data : str
+        The data payload received within POST requests sent to our API.
+    model : <class: sklearn.estimator>
+        An sklearn model object.
+
+    Returns
+    -------
+    list
+        A 1-D python list containing the model prediction.
+
+    """
+    # Data preprocessing.
+    prep_data = _preprocess_data(data)
+    # Perform prediction with model and preprocessed data.
+    prediction = model.predict(prep_data)
+    # Format as list for output standerdisation.
+    return prediction[0].tolist()
